086125730114a1db8237fbd2c993f7fac9e7172c
Neil Berget <neil.berget@gmail.com>
2014-10-01 15:06:49 -0700

    Do not filter exceptions through Rails.backtrace_cleaner

--- lib/exception_notifier/email_notifier.rb
+++ lib/exception_notifier/email_notifier.rb
@@ -65,11 +65,13 @@ module ExceptionNotifier
           end
 
           def clean_backtrace(exception)
-            if defined?(Rails) && Rails.respond_to?(:backtrace_cleaner)
-              Rails.backtrace_cleaner.send(:filter, exception.backtrace)
-            else
-              exception.backtrace
-            end
+            exception.backtrace
+
+            # if defined?(Rails) && Rails.respond_to?(:backtrace_cleaner)
+            #   Rails.backtrace_cleaner.send(:filter, exception.backtrace)
+            # else
+            #   exception.backtrace
+            # end
           end
 
           helper_method :inspect_object
